017b31803530e56eeab2d34be69ac7a7ac8891ef
Davis-Cedrick Baseka <base0005@algonquinlive.com>
2023-07-20 17:51:26 -0400

    Complete

--- routes/api.js
+++ routes/api.js
@@ -2,9 +2,11 @@
 const router=require("express").Router();
 const fs=require("fs");
 const uuid=require("../helpers/uuid");
+const path = require("path")
 
 router.get("/notes", (req, res) => {
-    let Noted=JSON.parse(fs.readFileSync("../db/db.json", "utf8"));
+    let Noted=JSON.parse(
+        fs.readFileSync(path.resolve(__dirname, "../db/db.json"), "utf8"));
     return res.json(Noted);
 });
 
@@ -12,10 +14,14 @@ router.post("/notes",(req,res)=>{
 let ReqBody=req.body;
 ReqBody.id=uuid();
 
-let Noted=JSON.parse(fs.readFileSync("../db/db.json", "utf8"));
+let Noted=JSON.parse(
+    fs.readFileSync(path.resolve(__dirname, "../db/db.json"), "utf8"));
 Noted.push(ReqBody);
 
-fs.writeFileSync("../db/db.json", JSON.stringify(Noted));
+fs.writeFileSync(
+    path.resolve(__dirname, "../db/db.json"), 
+    JSON.stringify(Noted)
+)
 return res.json(Noted);
 });
 
